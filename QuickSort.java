70a7bb724e07c500157dfe14cc057279c14eb696
GermianeLee <jermaine.trompetter@gmail.com>
2025-08-11 13:49:47 +0200

    QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,27 @@
+public class QuickSort {
+    public void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1); // Sort left sub-array
+            quickSort(arr, pivotIndex + 1, high); // Sort right sub-array
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+}
